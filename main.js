9a0236af6d0cbfd9caaa6381443e5aed612522d4
Alexander <hash_nam@mail.ru>
2023-08-20 02:59:44 +0600

    shops map select

--- main.js
+++ main.js
@@ -1,4 +1,53 @@
-var swiper = new Swiper(".mySwiper", {
+// api YaMap
+
+let center = [48.8866527839977, 2.34310679732974];
+
+function init() {
+  let map = new ymaps.Map("map", {
+    center: center,
+    zoom: 17,
+  });
+
+  map.controls.remove("geolocationControl"); // удаляем геолокацию
+  map.controls.remove("searchControl"); // удаляем поиск
+  map.controls.remove("trafficControl"); // удаляем контроль трафика
+  map.controls.remove("typeSelector"); // удаляем тип
+  map.controls.remove("fullscreenControl"); // удаляем кнопку перехода в полноэкранный режим
+  map.controls.remove("zoomControl"); // удаляем контрол зуммирования
+  map.controls.remove("rulerControl"); // удаляем контрол правил
+  map.behaviors.disable(["scrollZoom"]); // отключаем скролл карты (опционально)
+}
+
+// custom select
+
+let select = function () {
+  let selectHeader = document.querySelectorAll(".select__header");
+  let selectItem = document.querySelectorAll(".select__item");
+
+  selectHeader.forEach((item) => {
+    item.addEventListener("click", selectToggle);
+  });
+
+  selectItem.forEach((item) => {
+    item.addEventListener("click", selectChoose);
+  });
+
+  function selectToggle() {
+    this.parentElement.classList.toggle("is-active");
+  }
+
+  function selectChoose() {
+    let text = this.innerText,
+      select = this.closest(".select"),
+      currentText = select.querySelector(".select__current");
+    currentText.innerText = text;
+    select.classList.remove("is-active");
+  }
+};
+
+// swipe panel
+
+let swiper = new Swiper(".mySwiper", {
   spaceBetween: 30,
   navigation: {
     nextEl: ".swiper-button-next",
@@ -10,3 +59,6 @@ var swiper = new Swiper(".mySwiper", {
   mousewheel: true,
   keyboard: true,
 });
+
+select();
+ymaps.ready(init);
